15f188fda0f2b744e580aac2643afc6786aff55a
Dmitry4705410 <106599036+Dmitry4705410@users.noreply.github.com>
2023-08-08 16:39:40 +0300

    menuTop
    Верстка меню

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Главная</title>
+    <? include ('global-link.php');?>
+</head>
+<body>
+<? include("menuTop/top-menu.php");?>  
+</body>
+</html>
---
+++ menuTop/top-menu.php
@@ -0,0 +1,32 @@
+<link rel="stylesheet" href="/menuTop/top-menu.css">
+   <div class="top-menu">
+    <div class="menuBlock">
+      <div class="menu-block-left">
+       <a href="/">
+        <div class="logo-top-menu">
+         <div class="icon-top-menu"><img src="/icon/logo.svg" alt=""></div>
+          <div class="text-top-menu">система тестирования АГУ</div>
+        </div>
+        </a>
+        <div class="menu-elements">
+            <a href="#"><div class="element-menu">
+                главная
+            </div></a>
+            <a href="#"><div class="element-menu">
+                новости
+            </div></a>
+            <a href="#"><div class="element-menu">
+                о проекте
+            </div></a>
+        </div>
+        </div>
+        <div class="menu-info-right">
+            <div class="name-info">Толокнов Дмитрий</div>
+            <div class="dop-info">
+            <a href="#"><div class="dop-info-elem">Профиль</div></a>
+              <div class="dop-info-elem" >Выйти</div>
+            
+            </div>
+        </div>
+    </div>
+</div>
